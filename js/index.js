037f04bb66271801021b2543ba3b356f46a5977d
Mestag <79472510+Mestag@users.noreply.github.com>
2021-08-05 06:58:00 -0300

    yjh

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
+let button = document.getElementById("click")
+let accordion = document.getElementById("accordionFlushExample")
+console.log(button.checked)
+console.log("algo")
 
+button.onclick = function() {
+if (button.checked) {
+    accordion.style.display="none"
+    console.log("esconderse")
+} else {
+    accordion.style.display = "block"
+    console.log("abrirse")
+}
+}
